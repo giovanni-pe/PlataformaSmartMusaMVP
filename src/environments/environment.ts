4fbf90559b207dac648682650b07bc8128c50f14
giovanni <gkpe24@gmail.com>
2024-09-12 00:42:25 -0500

    add https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiBaseUrl: 'http://52.91.196.243:8000/api'  // La URL de tu backend en desarrollo
+    apiBaseUrl: 'https://52.91.196.243/api'  // La URL de tu backend en desarrollo
   };
   
